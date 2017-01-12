e88b60fe98a68015f89b24ca0506904dfce1ace9
Steve Sloka <slokas@upmc.edu>
2017-01-12 15:13:14 -0500

    wip

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -27,7 +27,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"sync"
@@ -51,7 +50,7 @@ var (
 
 func init() {
 	flag.BoolVar(&printVersion, "version", false, "Show version and quit")
-	flag.StringVar(&baseImage, "baseImage", "upmcenterprises/docker-elasticsearch-kubernetes:5.1.1", "Base image to use when spinning up the elasticsearch components.")
+	flag.StringVar(&baseImage, "baseImage", "upmcenterprises/docker-elasticsearch-kubernetes:5.1.1_2", "Base image to use when spinning up the elasticsearch components.")
 	flag.StringVar(&kubeCfgFile, "kubecfg-file", "", "Location of kubecfg file for access to kubernetes master service; --kube_master_url overrides the URL part of this; if neither this nor --kube_master_url are provided, defaults to service account tokens")
 	flag.StringVar(&masterHost, "masterhost", "http://127.0.0.1:9005", "Full url to k8s api server")
 	flag.Parse()
@@ -86,9 +85,9 @@ func Main() int {
 	// Kick it off
 	controller.Run()
 
-	// Watch for events that add, modify, or delete ElasticSearchCluster definitions and
+	// Watch for events that add, modify, or delete ElasticSearchCluster definitions andlog
 	// process them asynchronously.
-	log.Println("Watching for elastic search events...")
+	logrus.Info("Watching for elastic search events...")
 	wg.Add(1)
 	processor.WatchElasticSearchClusterEvents(doneChan, &wg)
 
@@ -97,7 +96,7 @@ func Main() int {
 	for {
 		select {
 		case <-signalChan:
-			log.Printf("Shutdown signal received, exiting...")
+			logrus.Error("Shutdown signal received, exiting...")
 			close(doneChan)
 			wg.Wait()
 			os.Exit(0)
