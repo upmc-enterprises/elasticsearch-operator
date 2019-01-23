a6904147ed2b4b97e36878c2ef96c3e371398052
Steve Sloka <steve@stevesloka.com>
2019-01-22 22:07:01 -0500

    Revert "Fix slice panic"

--- pkg/processor/processor.go
+++ pkg/processor/processor.go
@@ -250,12 +250,10 @@ func (p *Processor) processPodEvent(c *v1.Pod) error {
 
 	// Set the policy to retain
 	name := c.Labels["component"]
+	name = name[14:len(name)]
 
-	if len(name) > 14 {
-		name = name[14:len(name)]
+	p.k8sclient.UpdateVolumeReclaimPolicy(p.clusters[fmt.Sprintf("%s-%s", name, c.ObjectMeta.Namespace)].ESCluster.Spec.Storage.VolumeReclaimPolicy, c.ObjectMeta.Namespace, name)
 
-		p.k8sclient.UpdateVolumeReclaimPolicy(p.clusters[fmt.Sprintf("%s-%s", name, c.ObjectMeta.Namespace)].ESCluster.Spec.Storage.VolumeReclaimPolicy, c.ObjectMeta.Namespace, name)
-	}
 	return nil
 }
 
