d90f149a4bcf0b7ffe99f0f351ce671c91104fe6
Naredula Janardhana Reddy <naredula.jana@gmail.com>
2019-01-24 12:51:15 +0530

    removed import.

--- pkg/k8sutil/scaling_test.go
+++ pkg/k8sutil/scaling_test.go
@@ -2,7 +2,7 @@ package k8sutil
 
 import (
 	"testing"
-	"github.com/upmc-enterprises/elasticsearch-operator/pkg/k8sutil"
+
 )
 
 
@@ -16,21 +16,21 @@ const (
    incase if testing function does not return the error then the unit test should fail. 
 */
 func Test_scaling_change_setting(t *testing.T) {
-	err := k8sutil.ES_change_settings(NonExistingIP, "1m", "request"); 
+	err := ES_change_settings(NonExistingIP, "1m", "request"); 
 	if (err == nil){
 		t.Errorf("Scaling unittest change setting failed");
 	}
 }
 
 func Test_check_for_green(t *testing.T) {
-	err := k8sutil.ES_checkForGreen(NonExistingIP); 
+	err := ES_checkForGreen(NonExistingIP); 
 	if (err == nil){
 		t.Errorf("Scaling unittest check_for_green failed");
 	}
 }
 
 func Test_check_for_nodeUp(t *testing.T) {
-	err := k8sutil.ES_checkForNodeUp(NonExistingIP, NonExistingPOD, 5); 
+	err := ES_checkForNodeUp(NonExistingIP, NonExistingPOD, 5); 
 	if (err == nil){
 		t.Errorf("Scaling unittest check_for_nodeUp failed");
 	}
